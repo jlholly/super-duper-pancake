ed998055bddddb7fbd0ae646219d26e4cae40f8d
Josh <jlholly@users.noreply.github.com>
2020-05-20 08:52:53 -0500

    Picturefill.js, Object-fit-images polyfill

--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
 import './styles/main.scss';
 import '../node_modules/picturefill/dist/picturefill.min.js';
+import '../node_modules/object-fit-images/dist/ofi.min.js';
+
+document.createElement("picture");
