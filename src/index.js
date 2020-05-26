9907819f2cdc39b2aa9119ba6902b2e2a273ccaa
Josh <jlholly@users.noreply.github.com>
2020-05-26 12:07:00 -0500

    Legacy Support
    Fixes for IE 11

--- src/index.js
+++ src/index.js
@@ -2,4 +2,12 @@ import './styles/main.scss';
 import '../node_modules/picturefill/dist/picturefill.min.js';
 import '../node_modules/object-fit-images/dist/ofi.min.js';
 
+// Create Picture Element for Legacy Browsers
 document.createElement("picture");
+
+// Add UserAgent data to HTML tag. Can target for browser specific fixes.
+var doc = document.documentElement;
+doc.setAttribute('data-useragent', navigator.userAgent);
+
+// CSS object-fit polyfill
+objectFitImages();
